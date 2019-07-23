d22866a396a34c31be66613e6f5bc6fcc3a7435e
github username <github email address>
2019-07-23 16:39:14 -0700

    local host request

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://tabletopargame.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'localhost:3000' ]
+  config.action_cable.allowed_request_origins = [ 'http://localhost:3000' ]
   config.web_socket_server_url = "wss://tabletopargame.herokuapp.com/cable"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
