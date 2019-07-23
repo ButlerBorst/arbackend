8d818e357720f04aba5090284f3a0b938493fd0f
github username <github email address>
2019-07-23 16:33:04 -0700

    crazyness

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_cable.allowed_request_origins = ['localhost:300']
   config.action_mailer.perform_caching = false
-
+  config.action_cable.url = 'wss://tabletopargame.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://tabletopargame.herokuapp.com' ]
+  config.web_socket_server_url = "wss://tabletopargame.herokuapp.com/cable"
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
